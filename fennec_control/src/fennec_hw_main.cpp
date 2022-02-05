2e8731ac01f5de603618e212ba98ad0413a47f67
Lukas Evers <s0570965@htw-berlin.de>
2022-02-05 14:20:49 +0100

    Create fennec_hardware_interface based on ros_control_boilerplate

---
+++ fennec_control/src/fennec_hw_main.cpp
@@ -0,0 +1,24 @@
+#include <ros_control_boilerplate/generic_hw_control_loop.h>
+#include <fennec_control/fennec_hw_interface.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "fennec_hw_interface");
+  ros::NodeHandle nh;
+
+  // NOTE: We run the ROS loop in a separate thread as external calls such
+  // as service callbacks to load controllers can block the (main) control loop
+  ros::AsyncSpinner spinner(3);
+  spinner.start();
+
+  // Create the hardware interface specific to your robot
+  boost::shared_ptr<fennec_ns::FennecHWInterface> fennec_hw_interface
+    (new fennec_ns::FennecHWInterface(nh));
+  fennec_hw_interface->init();
+
+  // Start the control loop
+  ros_control_boilerplate::GenericHWControlLoop control_loop(nh, fennec_hw_interface);
+  control_loop.run();  // Blocks until shutdown signal recieved
+
+  return 0;
+}
